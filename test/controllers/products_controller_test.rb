64f8ba6e27875525cd2de5926fcffcfc6613810a
lkg0dzre <lkg0dzre@gmail.com>
2014-07-08 16:06:27 +0400

    Playtime: date and time at sidebar, functional test for product controller (Task C: Catalog Display)

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,9 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '.list_description dt', 'Programming Ruby 1.9'
+    assert_select '.list_description dd', 3
+    assert_select '.list_actions a', 9
   end
 
   test "should get new" do
