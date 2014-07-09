1ccc19688f58fc9ee1d488d35ce29a5c72d8675f
lkg0dzre <lkg0dzre@gmail.com>
2014-07-10 02:22:07 +0400

    Playtime: Views Count (Task D: Cart Creation)

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,6 +28,7 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.line_items.build(product: product)
+    session[:store_index_views_count] = 0
 
     respond_to do |format|
       if @line_item.save
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,8 @@
 class StoreController < ApplicationController
   def index
+    session[:store_index_views_count] ||= 0
+    session[:store_index_views_count] += 1
+    @views_count = session[:store_index_views_count]
     @products = Product.order(:title)
   end
 end
