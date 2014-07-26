61b2f20a1630fd2bbb52347cc594050573917558
lkg0dzre <lkg0dzre@gmail.com>
2014-07-27 03:21:08 +0400

    Iteration I3: Limiting Access (Task I: Logging In)

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
   include CurrentCart
   before_action :set_cart
+  skip_before_action :authorize
 
   def index
     session[:store_index_views_count] ||= 0
